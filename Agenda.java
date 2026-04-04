3061b311a4ff4d24ae07ca0c459ef406a8fb79e0
eliel <floreseliel55@gmail.com>
2026-04-03 17:03:51 -0700

    agregue arraylist para tener los contactos que tengan cierto tipo de telefono

--- Agenda.java
+++ Agenda.java
@@ -7,6 +7,29 @@ public class Agenda {
     public Agenda(){
         listaContacto = new ArrayList<Contacto>();
     }
+    public String toString() {
+        return "Agenda{" +
+                "listaContacto=" + listaContacto +
+                '}';
+    }
+    // Metodo que me devuelve una lista de contactos que tienen al menos un telefono del tipo dado (M o F) - Gael
+    public ArrayList<Contacto> getContactosPorTipoTelefono(char tipo){
+        // arreglo donde guardo los contactos que cumplen la condición
+        ArrayList<Contacto> resultado = new ArrayList<>();
+        // convierto el tipo a mayúscula para evitar problemas de comparación
+        char t = Character.toUpperCase(tipo);
+        // Recorro el arreglo de contactos y su lista
+        for (Contacto contacto : listaContacto) {
+            for (Telefono tel : contacto.getListaTelefonos()) {
+                // si el tipo de telefono coincide con el tipo dado, lo agrego al array y salgo para evitar agregar el mismo contacto varias veces
+                if (tel.getTipoTelefono() == t) {
+                    resultado.add(contacto);
+                    break;
+                }
+            }
+        }
+        return resultado;
+    }
 
 
 
